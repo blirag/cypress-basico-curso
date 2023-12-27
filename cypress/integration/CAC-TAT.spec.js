393d666c818f2282c7b1b92b3d0b5cd50dbea329
Beatriz <beatrizliragonzaga@gmail.com>
2023-12-26 21:17:29 -0300

    exercicios aula 10 e 11

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -6,19 +6,23 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 	});
 
 	it('preenche os campos obrigatórios e envia o formulário', () => {
+		cy.clock();
+
 		cy.get('#firstName').type('Beatriz');
 		cy.get('#lastName').type('Lira');
 		cy.get('#email').type('beatriz@gmail.com');
 		cy.get('#open-text-area').type('Dúvida sobre a Aula X do curso', {
 			delay: 5,
 		});
-
 		cy.contains('button', 'Enviar').should('be.visible').click();
 
 		cy.get('span.success').should('be.visible');
+		cy.tick(3000);
+		cy.get('span.success').should('not.be.visible');
 	});
 
 	it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+		cy.clock();
 		cy.get('#firstName').type('Beatriz').should('have.value', 'Beatriz');
 
 		cy.get('#lastName').type('Lira').should('have.value', 'Lira');
@@ -34,6 +38,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 		cy.get('button').click();
 
 		cy.get('span.error').should('be.visible');
+		cy.tick(3000);
+		cy.get('span.error').should('not.be.visible');
 	});
 
 	it('campo telefone continua vazio quando preenchido com valor não numérico', () => {
@@ -41,6 +47,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 	});
 
 	it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+		cy.clock();
 		cy.get('#firstName').type('Beatriz');
 		cy.get('#lastName').type('Lira');
 		cy.get('#email').type('beatriz@gmail');
@@ -49,6 +56,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 		cy.get('button').click();
 
 		cy.get('span.error').should('be.visible');
+		cy.tick(3000);
+		cy.get('span.error').should('not.be.visible');
 	});
 
 	it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
@@ -75,13 +84,21 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 	});
 
 	it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+		cy.clock();
 		cy.contains('button', 'Enviar').click();
 		cy.get('span.error').should('be.visible');
+		cy.tick(3000);
+		cy.get('span.error').should('not.be.visible');
 	});
 
 	it('envia o formulário com sucesso utilizando um comando customizado', () => {
-		cy.fillMandatoryFieldsAndSubmit();
-		cy.get('span.success').should('be.visible');
+		Cypress._.times(2, () => {
+			cy.clock();
+			cy.fillMandatoryFieldsAndSubmit();
+			cy.get('span.success').should('be.visible');
+			cy.tick(3000);
+			cy.get('span.success').should('not.be.visible');
+		});
 	});
 
 	it('seleciona um produto (YouTube) por seu texto', () => {
@@ -156,5 +173,39 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 	it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
 		cy.get('#privacy a').invoke('removeAttr', 'target').click();
 		cy.contains('Talking About Testing').should('be.visible');
+	});
+
+	it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+		cy.get('.success')
+			.should('not.be.visible')
+			.invoke('show')
+			.should('be.visible')
+			.and('contain', 'Mensagem enviada com sucesso.')
+			.invoke('hide')
+			.should('not.be.visible');
+		cy.get('.error')
+			.should('not.be.visible')
+			.invoke('show')
+			.should('be.visible')
+			.and('contain', 'Valide os campos obrigatórios!')
+			.invoke('hide')
+			.should('not.be.visible');
+	});
+
+	it('preenche a area de texto usando o comando invoke', () => {
+		const longText = Cypress._.repeat('0123456789', 20);
+		cy.get('#open-text-area')
+			.invoke('val', longText)
+			.should('have.value', longText);
+	});
+
+	it('faz uma requisição HTTP', () => {
+		cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html').then(
+			(response) => {
+				expect(response.status).to.equal(200);
+				expect(response.statusText).to.equal('OK');
+				expect(response.body).to.includes('CAC TAT');
+			}
+		);
 	});
 });
